4739665622c6dff7a2a05640dede29d992ecda11
Nguyễn Ngọc Tú Anh <117065875+anhhna231104@users.noreply.github.com>
2025-10-27 14:28:30 +0700

    Time: 11 ms (34.62%) | Memory: 13.8 MB (10.17%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        # i, n = 0, len(nums)
+        # while i<n:
+        #     if nums[i] == 0:
+        #         nums.pop(i)
+        #         nums.append(0)
+        #         n-=1
+        #     else:
+        #         i+=1
+        # return nums
+
+        j, n = 0, len(nums)
+        for i in range(n):
+            if nums[i]!=0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j+=1
+        return nums
+      
